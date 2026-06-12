c989f13a963bfdea92e788698666b00139dd387d
Suresh Dullu Polai <sureshdulupolai@gmail.com>
2026-06-12 22:46:03 +0530

    adding update on file py_1.py commenting unuse lines

--- py_1.py
+++ py_1.py
@@ -20,7 +20,7 @@ def accum(st):
             stringLetter += "-"
     return stringLetter
 
-# accum("abcd")
+# print(accum("abcd"))
 
 
 """
@@ -81,12 +81,13 @@ def find_short(s):
             count = len(i)
     return count
 
-# find_short("i want to travel the world writing code one day")
-# find_short("bitcoin take over the world maybe who knows perhaps")
+# print(find_short("i want to travel the world writing code one day"))
+# print(find_short("bitcoin take over the world maybe who knows perhaps"))
 
 
 """
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10 that are multiples of 3 or 5
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 Additionally, if the number is negative, return 0.
 Note: If a number is a multiple of both 3 and 5, only count it once.
@@ -101,10 +102,11 @@ def solution(number):
 
     return sum(count)
 
-# solution(10)
+# print(solution(10))
 
 """
-Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+Write a function that accepts an array of 10 integers (between 0 and 9)
+that returns a string of those numbers in the form of a phone number.
 create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
 """
 
@@ -121,11 +123,14 @@ def create_phone_number(n):
 
     return no
 
-create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # (123) 456-7890 
+# print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) # (123) 456-7890 
 # "({}{}{}) {}{}{}-{}{}{}{}".format(*n) => one line answer
 
 """
-Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+Usually when you buy something, you're asked whether your credit card number
+phone number or answer to your most secret question is still correct. 
+However, since someone could look over your shoulder, you don't want that shown on your screen. 
+Instead, we mask it.
 Your task is to write a function maskify, which changes all but the last four characters into '#'.
 
 "4556364607935616" --> "############5616"
@@ -145,19 +150,23 @@ def maskify(cc):
         return ("#" * len(cc[:-4])) + cc[-4:]
     
     return cc
-# maskify("SF$SDfgsd2eA")
+# print(maskify("SF$SDfgsd2eA"))
 
 
 """
-The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
-
-To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. 
+They would like your help with an application form that will tell prospective members which category they will be placed.
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. 
+In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
 
 Input
-Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+Input will consist of a list of pairs. 
+Each pair contains information for a single potential member. 
+Information consists of an integer for the person's age and an integer for the person's handicap.
 
 Output
-Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+Output will consist of a list of string values (in Haskell and C: Open or Senior) 
+stating whether the respective member is to be placed in the senior or open category.
 
 Example
 input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
