3f09435b067b71bb82ac84a2995d51eee4417971
Suresh Dullu Polai <sureshdulupolai@gmail.com>
2026-06-18 17:25:36 +0530

    question 2 with solve

--- py_3.py
+++ py_3.py
@@ -25,12 +25,81 @@ def alphanumeric(password: str) -> bool:
     
     return False
 
-tests = [
-    ("hello world_", False),
-    ("PassW0rd", True),
-    ("     ", False)
-]
-for s, b in tests:
-    print(alphanumeric(s), b)
+# tests = [
+#     ("hello world_", False),
+#     ("PassW0rd", True),
+#     ("     ", False)
+# ]
+# for s, b in tests:
+#     print(alphanumeric(s), b)
 
+
+"""
+Question:
+Recover the original secret string using a collection of ordered triplets.
+
+Each triplet represents the correct relative order of three characters in the final string. 
+Every character appears only once, and the given triplets contain enough information to reconstruct 
+the complete string.
+
+Example:
+
+Input:
+[
+  ['w', 'h', 'i'],
+  ['a', 't', 's'],
+  ['t', 'i', 's'],
+  ['t', 'u', 'p'],
+  ['h', 'a', 'p']
+]
+
+Meaning:
+w comes before h, h before i
+a comes before t, t before s
+t comes before i, i before s
+t comes before u, u before p
+h comes before a, a before p
+
+Output:
+"whatisup"
+"""
+
+from collections import defaultdict, deque
+
+def recover_secret(triplets):
+    graph = defaultdict(set)
+    indegree = {}
+
+    # nodes collect + indegree init
+    for triplet in triplets:
+        for ch in triplet:
+            indegree[ch] = 0
+
+    # build graph
+    for triplet in triplets:
+        a, b, c = triplet
+
+        if b not in graph[a]:
+            graph[a].add(b)
+            indegree[b] += 1
+
+        if c not in graph[b]:
+            graph[b].add(c)
+            indegree[c] += 1
+
+    # queue for zero indegree
+    q = deque([node for node in indegree if indegree[node] == 0])
+
+    result = []
+
+    while q:
+        node = q.popleft()
+        result.append(node)
+
+        for nei in graph[node]:
+            indegree[nei] -= 1
+            if indegree[nei] == 0:
+                q.append(nei)
+
+    return "".join(result)
 
